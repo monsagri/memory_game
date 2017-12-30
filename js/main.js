3bd10e0390b0cc729bad13214b4d53a580307289
Fabian Feldberg <feldbergf@gmail.com>
2017-12-30 15:13:59 +0000

    Added more game logic (Unit 8)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 console.log("Up and running!");
-var cardOne = "King"
-var cardTwo = "King"
-var cardThree = "Queen"
-var cardFour = "Queen"
-console.log("User flipped" + " " + cardOne)
+
+ var cards = ["queen","queen","king","king",];
+ var cardsInPlay = [];
+ var cardOne;
+ var cardOne = cards[0];
+ var cardTwo;
+ cardsInPlay.push(cardOne);
+ console.log("User flipped" + " " + cardOne);
+ var cardTwo = cards[2];
+ cardsInPlay.push(cardTwo);
+  console.log("User flipped" + " " + cardTwo);
+
+ if (cardsInPlay.length === 2) {
+ 	console.log("checking for match");
+ 	if (cardsInPlay[0] === cardsInPlay[1]) {
+ 		alert("Score! You found a match!");
+ 	} else {
+ 		alert("Try Again.");
+ 	}
+ }
