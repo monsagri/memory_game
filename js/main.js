d4d690bb18dedd1034c1d7d1274d48ec95267026
Fabian Feldberg <feldbergf@gmail.com>
2018-01-03 16:38:19 +0000

    Added more cards available under CC

--- js/main.js
+++ js/main.js
@@ -3,23 +3,93 @@ console.log("Up and running!");
 // Cards
  var cards = [ {
  	rank:"queen",
- 	suit:"diamonds",
- 	cardImage:"images/queen-of-diamonds.png",
+ 	suit:"clubs",
+ 	cardImage:"images/cards/queen-of-clubs.png",
  },
  {
  	rank:"queen",
- 	suit:"hearts",
- 	cardImage:"images/queen-of-hearts.png",
+ 	suit:"spades",
+ 	cardImage:"images/cards/queen-of-spades.png",
  },
  {
  	rank:"king",
- 	suit:"diamonds",
- 	cardImage:"images/king-of-diamonds.png",
+ 	suit:"clubs",
+ 	cardImage:"images/cards/king-of-clubs.png",
  },
  {
  	rank:"king",
- 	suit:"hearts",
- 	cardImage:"images/king-of-hearts.png",
+ 	suit:"spades",
+ 	cardImage:"images/cards/king-of-spades.png",
+ },
+ {
+	rank:"jack",
+ 	suit:"clubs",
+ 	cardImage:"images/cards/jack-of-clubs.png",
+ },
+ {
+ 	rank:"jack",
+ 	suit:"spades",
+ 	cardImage:"images/cards/jack-of-spades.png",
+ },
+ {
+	rank:"10",
+ 	suit:"clubs",
+ 	cardImage:"images/cards/10-of-clubs.png",
+ },
+ {
+	rank:"10",
+ 	suit:"spades",
+ 	cardImage:"images/cards/10-of-spades.png",
+ },
+ {
+	rank:"9",
+ 	suit:"clubs",
+ 	cardImage:"images/cards/9-of-clubs.png",
+ },
+ {
+	rank:"9",
+ 	suit:"spades",
+ 	cardImage:"images/cards/9-of-spades.png",
+ },
+ {
+	rank:"8",
+ 	suit:"clubs",
+ 	cardImage:"images/cards/8-of-clubs.png",
+ },
+ {
+	rank:"8",
+ 	suit:"spades",
+ 	cardImage:"images/cards/8-of-spades.png",
+ },
+ {
+	rank:"7",
+ 	suit:"clubs",
+ 	cardImage:"images/cards/7-of-clubs.png",
+ },
+ {
+	rank:"7",
+ 	suit:"spades",
+ 	cardImage:"images/cards/7-of-spades.png",
+ },
+ {
+	rank:"6",
+ 	suit:"clubs",
+ 	cardImage:"images/cards/6-of-clubs.png",
+ },
+ {
+	rank:"6",
+ 	suit:"spades",
+ 	cardImage:"images/cards/6-of-spades.png",
+ },
+ {
+	rank:"5",
+ 	suit:"spades",
+ 	cardImage:"images/cards/5-of-spades.png",
+ },
+ {
+	rank:"5",
+ 	suit:"clubs",
+ 	cardImage:"images/cards/5-of-clubs.png",
  }
  ];
 
@@ -30,9 +100,8 @@ console.log("Up and running!");
  // Check if cards match and empty the array after checking
 var checkForMatch = function() {
 	var flippedCards = document.querySelectorAll("img[data-status = 'flipped']");
-	//var flippedCard1 = document.querySelectorAll("img[data-status = 'flipped']")[1];
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 		//Find a way to remove flipped cards   -> set status to found
 			scoreString += 5;
 			document.getElementById('scoredisplay').innerHTML = scoreString;
@@ -40,14 +109,23 @@ var checkForMatch = function() {
 				flippedCards[i].setAttribute('data-status', "found");
 				flippedCards[i].removeEventListener('click', flipCard);
 			};
+		/*Check to see if any cards remain unflipped and check for Highscores if not
+			var remainingCards = document.querySelectorAll("img[data-status = 'unflipped']");
+			console.log(remainingCards)
+			if (remainingCards == "") {
+				checkForHighscore();
+			} else {
+
+			};
+		*/
 			cardsInPlay = [];
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 		// Add a way to unflip wrong cards -> set status back to unflipped and change src
 			scoreString -= 2;
 			document.getElementById('scoredisplay').innerHTML = scoreString;
 			for (i = 0; i <flippedCards.length; i++) {
-				flippedCards[i].setAttribute('src', "images/back.png");
+				flippedCards[i].setAttribute('src', "images/cards/back.png");
 				flippedCards[i].setAttribute('data-status', "unflipped");
 			};
 			cardsInPlay = [];
@@ -70,9 +148,10 @@ var flipCard = function() {
 var createBoard = function() {
 	for (var i = 0; i < cards.length; i++) {
 			cardElement = document.createElement('img');
-			cardElement.setAttribute('src', "images/back.png");
+			cardElement.setAttribute('src', "images/cards/back.png");
 			cardElement.setAttribute('data-id', i);
 			cardElement.setAttribute('data-status', "unflipped");
+			cardElement.setAttribute('class', "card");
 			cardElement.addEventListener('click', flipCard);
 			document.getElementById('game-board').appendChild(cardElement);
 
