5fbc0968ec0ab12d3c3f35130f31c1da6d308800
Fabian Feldberg <feldbergf@gmail.com>
2018-01-03 18:11:29 +0000

    Figured out and added random placement of cards

--- js/main.js
+++ js/main.js
@@ -4,6 +4,50 @@ console.log("Up and running!");
  var cardAmount = cards.length;
  var scoreString = parseInt(document.getElementById('scoredisplay').innerHTML);
 
+ //Random number generation from MDN
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Create the board
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+			cardElement = document.createElement('img');
+			cardElement.setAttribute('src', "images/cards/back.png");
+			cardElement.setAttribute('data-id', i);
+			cardElement.setAttribute('data-status', "unflipped");
+			cardElement.setAttribute('class', "card");
+			cardElement.addEventListener('click', flipCard);
+			document.getElementById('game-board').appendChild(cardElement);
+	};
+	
+};
+
+// Use insertChild to move each card around once to a random position
+	//Create a random number between 0 and currentBoard.length as new position
+var shuffleCards = function() {
+	var currentBoard = document.querySelectorAll("img[data-id]");
+	for (i = 0; i < currentBoard.length; i++) {
+			var randomNumber = getRandomInt(0, currentBoard.length);
+			console.log(randomNumber);
+			var cardToShuffle = document.getElementById('game-board').childNodes[i];
+			var randomCardLocation = document.getElementById('game-board').childNodes[randomNumber];
+			document.getElementById('game-board').insertBefore(cardToShuffle, randomCardLocation);
+	};
+}
+
+ // flipping Cards 
+var flipCard = function() {
+	cardId = this.getAttribute('data-id');
+	this.setAttribute('data-status', "flipped");
+ 	console.log("Player flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+ 	cardsInPlay.push(cards[cardId].rank);
+ 	this.setAttribute('src', cards[cardId].cardImage);
+ 	if (cardsInPlay.length === 2) {
+ 		checkForMatch();
+ 	};	
+};
+
  // Check if cards match and empty the array after checking
 var checkForMatch = function() {
 	var flippedCards = document.querySelectorAll("img[data-status = 'flipped']");
@@ -38,31 +82,6 @@ var checkForMatch = function() {
 			cardsInPlay = [];
 	}
 }
- // flipping Cards 
-var flipCard = function() {
-	cardId = this.getAttribute('data-id');
-	this.setAttribute('data-status', "flipped");
- 	console.log("Player flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
- 	cardsInPlay.push(cards[cardId].rank);
- 	this.setAttribute('src', cards[cardId].cardImage);
- 	if (cardsInPlay.length === 2) {
- 		checkForMatch();
- 	};	
-};
-//Create the board
-var createBoard = function() {
-	for (var i = 0; i < cards.length; i++) {
-			cardElement = document.createElement('img');
-			cardElement.setAttribute('src', "images/cards/back.png");
-			cardElement.setAttribute('data-id', i);
-			cardElement.setAttribute('data-status', "unflipped");
-			cardElement.setAttribute('class', "card");
-			cardElement.addEventListener('click', flipCard);
-			document.getElementById('game-board').appendChild(cardElement);
-	};
-};
-
-createBoard();
 
 //Reset Board and Score function
 var reset = function () {
@@ -107,3 +126,6 @@ var checkForHighscore = function() {
 		alert("Better luck next time!");
 	}
 };
+
+createBoard();
+shuffleCards();
